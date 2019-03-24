3a9da8b9ba7a69560997257c2e86072ec7b8bc1b
Ishmeher Singh Ahuja <34440761+black-immortal@users.noreply.github.com>
2019-03-24 13:22:06 +0530

    Linear SVM - Dataset1

---
+++ SVM_Test.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix
+
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data"
+colnames = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11']
+data = pd.read_csv(url, names=colnames)
+
+X = data.drop('1', axis=1)
+X = X.drop('7', axis=1)
+X = X.drop('11', axis=1)
+Y = data['11']
+
+trainX, testX, trainY, testY = train_test_split(X, Y, test_size=0.20)
+
+classifier = SVC(kernel='linear')
+classifier.fit(trainX, trainY)
+
+Y_pred = classifier.predict(testX)
+
+print(confusion_matrix(testY, Y_pred))
